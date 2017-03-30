c66b5e1be6e1e44d7b18841568aa1463ba09e4c8
AnkurBeginning <201551016.iiitvadodara.ac.in@gmail.com>
2017-03-30 11:51:00 +0530

    feat:Intialize reference to View, ListView and its adapter

--- app/src/main/java/com/example/android/arvindo/MainActivity.java
+++ app/src/main/java/com/example/android/arvindo/MainActivity.java
@@ -5,13 +5,50 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String ANONYMOUS = "anonymous";
+    public static final int DEFAULT_MSG_LENGTH_LIMIT = 1000;
+
+    private ListView mMessageListView;
+    private MessageAdapter mAdapter;
+    private ProgressBar mProgressBar;
+    private ImageButton mImageButton;
+    private EditText mEditText;
+    private Button mSendButton;
+
+    private String mUserName;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mUserName = ANONYMOUS;
+
+        //Intilize Reference to view
+        mMessageListView =(ListView)findViewById(R.id.list_item_view);
+        mProgressBar=(ProgressBar)findViewById(R.id.progress_bar);
+        mImageButton=(ImageButton)findViewById(R.id.photo_picker);
+        mEditText=(EditText)findViewById(R.id.message_edit_text);
+        mSendButton=(Button)findViewById(R.id.send_button);
+
+        //Intilize Message ListView and its adapter
+        List<MessageBody> message = new ArrayList<>();
+
+        mAdapter =new MessageAdapter(this,R.layout.list_item,message);
+        mMessageListView.setAdapter(mAdapter);
+
     }
 
     @Override
