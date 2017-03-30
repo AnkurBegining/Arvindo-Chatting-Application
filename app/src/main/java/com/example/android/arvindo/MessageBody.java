7c1e8999d82787d0283ac77507bb909f4ccd6457
AnkurBeginning <201551016.iiitvadodara.ac.in@gmail.com>
2017-03-30 09:59:39 +0530

    feat:Added MessageBody class

---
+++ app/src/main/java/com/example/android/arvindo/MessageBody.java
@@ -0,0 +1,45 @@
+package com.example.android.arvindo;
+
+/**
+ * Created by Lenovo on 30-03-2017.
+ */
+
+public class MessageBody {
+
+    private String name;
+    private String text;
+    private String photoUrl;
+
+    public MessageBody(){
+
+    }
+
+    public MessageBody(String name, String text, String photoUrl){
+        this.name=name;
+        this.text=text;
+        this.photoUrl=photoUrl;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getText(){
+        return text;
+    }
+
+    public void setText(String text){
+        this.text = text;
+    }
+
+    public String getPhotoUrl(){
+        return photoUrl;
+    }
+    public void setPhotoUrl(String photoUrl){
+        this.photoUrl = photoUrl;
+    }
+}
